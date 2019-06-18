ae34f8cf7b0c636f6b3995aa53f096794e4ce7dc
LeandroCaballero <leandrorojo248@hotmail.com>
2019-06-17 22:54:17 -0300

    Cambio Simulador.py

--- Simulacion.py
+++ Simulacion.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
 import numpy as np
 from pandas import ExcelWriter
 from pandas import ExcelFile
@@ -5,14 +11,7 @@ import pandas as pd
 import random
 from datetime import datetime, date, time, timedelta
 
-# Importar herramientas graficas
 import sys
-
-from PyQt5.QtWidgets import *
-from PyQt5.uic.properties import QtGui
-
-#Importamos ventana.py
-import ventana
 
 import Franja 
 Franja=Franja.Franja
@@ -22,31 +21,98 @@ Materia= Materias.Materia
 aulasDosModulos=0
 aulasTresModulos=0
 
-class Simulacion(QMainWindow):
-    def __init__(self):
-        self.materiasNoAsignadasTresModulos=[]
-        self.materias=[]
-        self.materiasNoAsignadasDosModulos=[]
-        
-        #Incializamos nuestra app
-        QMainWindow.__init__(self)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-        # Instanciamos nuestra ventanas widget ventana
-        self.ventana = ventana.Ui_MainWindow()
-        self.ventana.setupUi(self)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(513, 432)
+        MainWindow.setStyleSheet("background-color: rgb(85, 255, 255);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.boton_simular = QtWidgets.QPushButton(self.centralwidget)
+        self.boton_simular.setGeometry(QtCore.QRect(220, 300, 101, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.boton_simular.setFont(font)
+        self.boton_simular.setStyleSheet("background-color: rgb(85, 170, 127);")
+        self.boton_simular.setObjectName("boton_simular")
+        self.franjaHoraria = QtWidgets.QComboBox(self.centralwidget)
+        self.franjaHoraria.setGeometry(QtCore.QRect(220, 120, 101, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.franjaHoraria.setFont(font)
+        self.franjaHoraria.setObjectName("franjaHoraria")
+        self.franjaHoraria.addItem("")
+        self.franjaHoraria.addItem("")
+        self.franjaHoraria.addItem("")
+        self.dia = QtWidgets.QComboBox(self.centralwidget)
+        self.dia.setGeometry(QtCore.QRect(220, 210, 101, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.dia.setFont(font)
+        self.dia.setObjectName("dia")
+        self.dia.addItem("")
+        self.dia.addItem("")
+        self.dia.addItem("")
+        self.dia.addItem("")
+        self.dia.addItem("")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 120, 161, 31))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(110, 210, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(210, 20, 121, 61))
+        font = QtGui.QFont()
+        font.setPointSize(35)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        # Eventos, cuando aprete el boton simular se ejecuta "simular"
-        self.ventana.boton_simular.connect(self.simular)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.boton_simular.clicked.connect(self.operacion)
 
-    def simular(self):
-            #Obtenemos los datos ingresados
-            franja = self.ventana.franjaHoraria.currentText()
-            dia = self.ventana.dia.currentText()
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.boton_simular.setText(_translate("MainWindow", "Simular"))
+        self.franjaHoraria.setCurrentText(_translate("MainWindow", "Mañana"))
+        self.franjaHoraria.setItemText(0, _translate("MainWindow", "Mañana"))
+        self.franjaHoraria.setItemText(1, _translate("MainWindow", "Tarde"))
+        self.franjaHoraria.setItemText(2, _translate("MainWindow", "Noche"))
+        self.dia.setItemText(0, _translate("MainWindow", "Lunes"))
+        self.dia.setItemText(1, _translate("MainWindow", "Martes"))
+        self.dia.setItemText(2, _translate("MainWindow", "Miercoles"))
+        self.dia.setItemText(3, _translate("MainWindow", "Jueves"))
+        self.dia.setItemText(4, _translate("MainWindow", "Viernes"))
+        self.label.setText(_translate("MainWindow", "Seleccione franja horaria"))
+        self.label_2.setText(_translate("MainWindow", "Seleccione día"))
+        self.label_3.setText(_translate("MainWindow", "SADA"))
 
-            # ejecutar la funcion: cambiá la variable, nose que queres hacer con esta variable
-            simulacion = Simulacion.simulacionFranjaDia(self,franja,dia)
-    
+    def operacion(self):
+        franja = self.franjaHoraria.currentText()
+        dia = self.dia.currentText()
+        sim= Ui_MainWindow()
+        sim.simulacionFranjaDia(franja,dia)
+        for x in sim.fran.dia.aulas:
+            print(x.descripcion)
+            for y in x.horas:
+                print("     ", y.hora)
+                print("         ", y.asignado)
+
     def simulacionFranjaDia(self, franja, dia):
         global aulasDosModulos
         global aulasTresModulos
@@ -104,7 +170,7 @@ class Simulacion(QMainWindow):
             while excelMateria.Año[i]==3 and excelMateria.Año[i]==5:
                 self.materias.append(Materia(excelMateria.CodigoMateria[i], excelMateria.Facultad[i], excelMateria.Carrera[i], excelMateria.Nombre[i], excelMateria.Año[i], excelMateria.Semestre[i], excelMateria.CantHs[i], excelMateria.CantAlumnos[i]))
                 i=i+1
-
+    
     def resolucionHoras(self, mt, flag):
         cantHs=mt.cantHs
         if cantHs==4:
@@ -164,32 +230,13 @@ class Simulacion(QMainWindow):
                 self.fran.dia.aulas[aleatorioAula].aceptaTresModulos=False
                 self.fran.dia.aulas[aleatorioAula].aceptaDosModulos=False
                 aulasTresModulos = aulasTresModulos - 1
-                
 
-
-                
-
-sim= Simulacion()
-sim.simulacionFranjaDia("Mañana", "Lunes")
-for x in sim.fran.dia.aulas:
-    print(x.descripcion)
-    for y in x.horas:
-        print("     ", y.hora)
-        print("         ", y.asignado)
-
-
-def iniciar():
-    # Instaciamos nuestro app por defecto esto no cambia
-    app = QApplication(sys.argv)
-
-    # Instanciamos nuestra ventana
-    ventana = Simulacion()
-    # Mostramos nuestra app
-    ventana.show()
-
-    #Controlamos el cierre de la app
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
 
-
-if __name__ == '__main__':
-    iniciar()
